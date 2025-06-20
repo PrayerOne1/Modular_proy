a20f0acf323b1907815bee9fe1e5512ff38c16b5
PrayerOne1 <160205286+PrayerOne1@users.noreply.github.com>
2025-06-19 22:17:10 -0600

    Add files via upload

---
+++ Ejercicios.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 27 22:17:14 2025
+
+@author: PrayerOne
+"""
+
+def ejercicios():    
+    def ejercicio1():
+        a = 0
+        b = 1
+        fib = []
+        fib.append(a)
+        fib.append(b)
+        while fib.len + 1:
+            c = sum(a,b)
+            fib.append(c)
+            
+        for i in range (fib):
+            print (fib)
+            i+=1
+            
+    def ejercicio2():
+        num = int(input("Enter a number:\n"))
+        numb = []
+        for i in num:
+            numb.append(i)
+            
+        
+    ejercicio1()
+    ejercicio2()
+ejercicios()
